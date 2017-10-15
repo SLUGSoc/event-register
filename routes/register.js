0c055bee41c0abc20fdf65916f5dfca1f2ae07ba
Rob Ede <robjtede@icloud.com>
2017-10-15 04:12:27 +0100

    add manual registering

--- routes/register.js
+++ routes/register.js
@@ -6,29 +6,67 @@ const { Member, Register, Event } = require('../db/models')
 
 const init = router => {
   router.get('/register/:id', register)
+  router.get('/register/:id/end', end)
+  router.get('/register/:id/mark/:member/in', markIn)
+  router.get('/register/:id/mark/:member/out', markOut)
 }
 
 const register = async ctx => {
   await ctx.render('register', {
-    register: groupBy(
-      await Register.findAll({
-        where: {
-          eventId: ctx.params.id
-        },
-        include: [
-          {
-            model: Member,
-            as: 'member'
-          },
-          {
-            model: Event,
-            as: 'event'
-          }
-        ]
-      }),
-      'event.name'
-    )
+    event: await Event.findById(ctx.params.id),
+    register: await Register.findAll({
+      where: {
+        eventId: ctx.params.id
+      },
+      include: [
+        {
+          model: Member,
+          as: 'member'
+        }
+      ]
+    })
   })
+}
+
+const end = async ctx => {
+  await Register.update(
+    {
+      present: false
+    },
+    {
+      where: {
+        eventId: ctx.params.id
+      }
+    }
+  )
+
+  ctx.redirect(`/register/${ctx.params.id}`)
+}
+
+const mark = async (eventId, memberId, present) => {
+  await Register.update(
+    {
+      present: present
+    },
+    {
+      where: {
+        eventId: eventId,
+        memberId: memberId
+      }
+    }
+  )
+}
+
+const markIn = async ctx => {
+  await mark(ctx.params.id, ctx.params.member, true)
+
+  ctx.redirect(`/register/${ctx.params.id}`)
+}
+
+const markOut = async ctx => {
+  await mark(ctx.params.id, ctx.params.member, false)
+
+  ctx.redirect(`/register/${ctx.params.id}`)
 }
 
 module.exports = {
